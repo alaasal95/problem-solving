fd26b26ec29d109d5179f45d3bb1a3dfa9c8e4a3
alaasal95 <74542065+alaasal95@users.noreply.github.com>
2020-11-16 15:36:47 +0300

    Add files via upload

---
+++ First one.py
@@ -0,0 +1,12 @@
+
+
+
+a=int(input('enter the first number:'))
+
+b=int(input('enter the second number'))
+if (a>b):
+    print('a is the lagrest ')
+elif (a<b):
+    print('b is the largest')
+elif (a==b):
+    print('a equal to b')
