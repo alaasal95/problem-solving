f659507830bd83e6ad5018916f3c21273dec71a1
alaasal95 <74542065+alaasal95@users.noreply.github.com>
2020-11-16 15:37:22 +0300

    Add files via upload

---
+++ enter your mark.py
@@ -0,0 +1,11 @@
+
+
+
+mark=int(input('Please enter your marks:'))
+
+if (mark <=100 and mark>=50 ):
+    print('pass')
+elif (mark <=100 and mark<50):
+    print('Fail')
+elif (mark >100 or mark <0):
+    print('wrong number!...')
