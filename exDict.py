e48c5b81d93bdacc39fc02dccec7e233c7d49229
alaasal95 <74542065+alaasal95@users.noreply.github.com>
2020-11-16 15:38:13 +0300

    Add files via upload

---
+++ exDict.py
@@ -0,0 +1,7 @@
+
+
+
+days={'s':'sunday','m':'monday','t':'tuesday','w':'wednesday','th':'thursday','f':'friday','sa':'saturday'}
+
+day='fm'
+print(days.get(day,'please enter valid number'))
---
+++ justex.py
@@ -0,0 +1,6 @@
+
+
+cityes={1:'baghdad',2:'wasit',3:'najaf',4:'karbalaa',5:'babil'}
+
+city=44
+print(cityes.get(city,'please enter valid number'))
